66bd55939fda1f2416687abad7836f761db8454a
DoggoFromWater <116671341+DoggoFromWater@users.noreply.github.com>
2022-11-22 21:34:23 +0200

    a

--- Code/3 - Exercises - Arrays.js
+++ Code/3 - Exercises - Arrays.js
@@ -16,7 +16,7 @@ let one = (arr) => {
     console.log(`after ${sum2}`)
 };
 
-let two = (arr,arr2) => {
+let two = (arr, arr2) => {
     for (let i=0;i<arr.length;i++) {
         for (let y=0;y<arr2.length;y++) {
             if (arr[i] === arr2[y]) {
@@ -26,3 +26,25 @@ let two = (arr,arr2) => {
     };
 };
 
+let three = (arr, arr2) => {
+    let arr3 = [];
+    for (let i=0;i<arr.length;i++) {
+        if (i % 2 == 0) {
+            arr3.push(parseFloat(arr[i])+parseFloat(arr2[i]))
+        }
+        else {arr3.push(arr[i]+arr2[i])}
+    };
+    console.log(arr3.join(" - "))
+};
+
+let four = (arr, num) => {
+    for (let i=0;i<num;i++) {
+        let a =arr.shift()
+        arr.push(a)
+    };
+    console.log(arr)
+};
+
+let five = () => {
+
+};
